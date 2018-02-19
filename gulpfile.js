10ecb39e8e3c53e7b68bab6f1673486357196784
cognitive-cake <nd.chernikov@gmail.com>
2018-02-19 21:54:29 +0300

    fix header title

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,7 @@ gulp.task('serve', function () {
     ui: false
   });
 
-  gulp.watch('sass/**/*.{scss,sass}', ['style-project']);
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
   gulp.watch('pug/**/*.pug', ['pug']);
   gulp.watch('js/*.js', ['babel']);
   gulp.watch('*.html').on('change', server.reload);
