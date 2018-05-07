c513afbb9b9ca8f3e2f630353dc9f16c0dedae49
Nick-ch <nd.chernikov@gmail.com>
2018-05-07 21:08:08 +0300

    add arrows for slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import canvasButtonRun from './canvasButton';
 import canvasPriceRun from './canvasPrice';
+// import sliderScreenshotsRun from './sliderScreenshots';
 
 canvasButtonRun();
 canvasPriceRun();
+// sliderScreenshotsRun();
---
+++ js/sliderScreenshots.js
@@ -0,0 +1,30 @@
+const sliderScreenshotsRun = () => {
+  $(document).ready(() => {
+    $('.slider-screenshots').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+        },
+      },
+      ],
+    });
+  });
+};
+
+export default sliderScreenshotsRun;
