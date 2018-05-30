f363f565817963db587e80e93bd3ad83accd8d03
cognitive-cake <nd.chernikov@gmail.com>
2018-05-30 22:25:29 +0300

    add widths for imgs in gallery

--- js/gallerySlider.js
+++ js/gallerySlider.js
@@ -1,6 +1,15 @@
 const renderGallerySlider = () => {
   $('#placeholder').html('work in progress');
-};
 
+  $('#slider-gallery-prev').click((e) => {
+    e.preventDefault();
+    alert('click on left btn');
+  });
+
+  $('#slider-gallery-next').click((e) => {
+    e.preventDefault();
+    alert('click on right btn');
+  });
+};
 
 export default renderGallerySlider;
